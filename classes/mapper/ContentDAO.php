3c2db9df2d4e382f5c9b6c3f1ebd60ae57e2841b
Acidphreack <64482975+Acidphreack@users.noreply.github.com>
2020-04-28 22:21:25 +0200

    Add files via upload

--- classes/mapper/ContentDAO.php
+++ classes/mapper/ContentDAO.php
@@ -27,15 +27,57 @@ class ContentDAO{
         $preStmt->bind_result($id, $page, $content, $description, $html_id);
         
         while($preStmt->fetch()){
-			$content = new Content($id, $page, $description, $content, $html_id);
+			$thisContent = new Content($id, $page, $description, $content, $html_id);
 
-			$contentList[] = $content;
+			$contentList[] = $thisContent;
         }
         
         $preStmt->free_result();
         $preStmt->close();
         
         return $contentList;
+    }
+
+    public function updateContent(Content $content){
+        $nummer = (int)$content->getNummer();
+        $pageNummer = (int)$content->getPageNummer();
+        $contentDescription = "".$content->getContentDescription();
+        $thisContent = "".$content->getContent();
+        $html_id = "".$content->getHtml_id();
+
+        $ok = false;
+        $sql = "UPDATE content
+                SET content.page = ?, content.content = ?, content.description = ?, content.html_id = ?
+                WHERE content.id = ?";
+        $preStmt = $this->dbConnect->prepare($sql);
+		$preStmt->bind_param("sssss", $pageNummer, $thisContent, $contentDescription, $html_id, $nummer);
+		$preStmt->execute();
+		$ok = $this->dbConnect->affected_rows;
+		$preStmt->free_result();
+        $preStmt->close();
+        
+        return $ok;
+    }
+
+    public function updateContentGivingAllParameters($contentNr, $pageNr, $description, $theContent, $html_id){
+        $nummer = (int)$contentNr;
+        $pageNummer = (int)$pageNr;
+        $contentDescription = "".$description;
+        $thisContent = "".$theContent;
+        $thishtml_id = "".$html_id;
+
+        $ok = false;
+        $sql = "UPDATE content
+                SET content.page = ?, content.content = ?, content.description = ?, content.html_id = ?
+                WHERE content.id = ?";
+        $preStmt = $this->dbConnect->prepare($sql);
+		$preStmt->bind_param("sssss", $pageNummer, $thisContent, $contentDescription, $thishtml_id, $nummer);
+		$preStmt->execute();
+		$ok = $this->dbConnect->affected_rows;
+		$preStmt->free_result();
+        $preStmt->close();
+        
+        return $ok;
     }
 
 }
