7e8ea0f089d1d0f1f321baa719c66dcce1687748
Acidphreack <64482975+Acidphreack@users.noreply.github.com>
2020-04-30 01:44:06 +0200

    Add files via upload

--- classes/commands/UserHomeCommand.php
+++ classes/commands/UserHomeCommand.php
@@ -1,6 +1,6 @@
 <?php
     namespace classes\commands;
-
+    use classes\model\User; // added um instanceof benutzen zu können.
     use classes\request\Request;
     use classes\response\Response;
     use classes\template\HtmlTemplateView;
@@ -8,9 +8,27 @@
 
     class UserHomeCommand implements Command{
         public function execute(Request $request, Response $response) {
+
+            if (isset($_SESSION['user'])){
+                $currentUser = unserialize($_SESSION['user']);
+            }
+            else{
+                // header to bye bye weil keine Session
+                header('location: index.php');
+            }
+            if (!($currentUser instanceof User)){
+                // header to bye bye weil session = wtf
+                header('location: index.php');
+            }
+            if ($currentUser->getStatus() != "user"){
+                header('location: index.php');
+                // header to bye bye weil kein user - glaub nun sollten alle möglichen unberechtigten rausgefiltert worden sein.
+            }
+
+
             $view = 'UserHome';
 
-            $userId = $_SESSION['userId'];
+            $userId = $currentUser->getId();
 
             $template = new HtmlTemplateView($view);
             
