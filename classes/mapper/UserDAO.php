0c6a8d0ed0606f431730ca2b17108b088e18604c
Acidphreack <64482975+Acidphreack@users.noreply.github.com>
2020-04-29 22:56:27 +0200

    Add files via upload

--- classes/mapper/UserDAO.php
+++ classes/mapper/UserDAO.php
@@ -125,33 +125,31 @@
             $user = null;
 
             if($this->exist($email)) {
-
                 if($this->passwordCheck($email, $password)) {
-                    
                     $sql = "SELECT id, displayname, status
                                 FROM user
-                                WHERE email = ?
-                                AND passwd = ?";
+                                WHERE email = ?";
+                              //AND passwd = ?"; -edited out this part- in der Datenbank liegt ja das verschlüsselte PW - dass ist niemal gleich dem unverschlüsseltem $password hier. und die überprüfung hast du ja eh schon in der if abfrage davor gemacht.
 
                     if(!$preStmt = $this->dbConnect->prepare($sql)){
                         echo "Fehler bei SQL-Vorbereitung (" . $this->dbConnect->errno . ")" . $this->dbConnect->error ."<br>";
                     } else {
-                        if(!$preStmt->bind_param("ss", $email, $password)){
+                        if(!$preStmt->bind_param("s", $email)){ // if(!$preStmt->bind_param("ss", $email, $password))
                             echo "Fehler beim Binding (" . $this->dbConnect->errno . ")" . $this->dbConnect->error ."<br>";
                         } else {
                             if(!$preStmt->execute()){
                                 echo "Fehler beim Ausführen (" . $this->dbConnect->errno . ")" . $this->dbConnect->error ."<br>";
                             } else {
+                                $preStmt->store_result(); // added wegen zwischeinizialisierung von $pagelist
                                 if(!$preStmt->bind_result($id, $displayname, $status)){
                                     echo "Fehler beim Ergebnis-Binding (" . $this->dbConnect->errno . ")" . $this->dbConnect->error ."<br>";
                                 } else {
                                     if($preStmt->fetch()){
 
                                         $pageDAO = new PageDAO();
-
                                         $pageList = $pageDAO->readPagesOfUserWithContent($id);
 
-                                        $user = new User($id, $displayname, $pageList, $status);
+                                        $user = new User($id, "", "", "", "", 0, $displayname, $status, $pageList);
                                     }
                                     $preStmt->free_result();
                                 }
@@ -163,5 +161,153 @@
                 return $user;
             }
         }
+
+
+
+
+
+        // added 29.04 -latenight
+
+
+        public function readUserByEmail($email){
+            $userEmail = "".$email;
+            $sql = "SELECT user.* 
+                    FROM user
+                    WHERE user.email = ?";
+            $preStmt = $this->dbConnect->prepare($sql);
+            $preStmt->bind_param("s", $userEmail);
+            $preStmt->execute();
+            $preStmt->store_result();
+            $preStmt->bind_result($id, $firstname, $lastname, $email, $passwd, $validation, $displayname, $status);
+            
+            $error = 0;
+            while($preStmt->fetch()){
+                $user = new User($id, $firstname, $lastname, $email, $passwd, $validation, $displayname, $status, null);
+                $error++;
+            }
+            
+            $preStmt->free_result();
+            $preStmt->close();
+            
+            if($error != 1){
+                return false;
+            }
+            return $user;
+        }
+
+        public function readUserWithPages($userId){
+            $uid = "".$userId;
+            $sql = "SELECT user.* 
+                    FROM user
+                    WHERE user.id = ?";
+            $preStmt = $this->dbConnect->prepare($sql);
+            $preStmt->bind_param("s", $uid);
+            $preStmt->execute();
+            $preStmt->store_result();
+            $preStmt->bind_result($id, $firstname, $lastname, $email, $passwd, $validation, $displayname, $status);
+            
+            $error = 0;
+            $pageDAO = new PageDAO;
+            while($preStmt->fetch()){
+                $pageList = $pageDAO->readPagesOfUser($id);
+                $user = new User($id, $firstname, $lastname, $email, $passwd, $validation, $displayname, $status, $pageList);
+                $error++;
+            }
+            
+            $preStmt->free_result();
+            $preStmt->close();
+            
+            if($error != 1){
+                return false;
+            }
+            return $user;
+        }
+
+        public function readUserWithPagesAndTheirContent($userId){
+            $uid = "".$userId;
+            $sql = "SELECT user.* 
+                    FROM user
+                    WHERE user.id = ?";
+            $preStmt = $this->dbConnect->prepare($sql);
+            $preStmt->bind_param("s", $uid);
+            $preStmt->execute();
+            $preStmt->store_result();
+            $preStmt->bind_result($id, $firstname, $lastname, $email, $passwd, $validation, $displayname, $status);
+            
+            $error = 0;
+            $pageDAO = new PageDAO;
+            while($preStmt->fetch()){
+                $pageList = $pageDAO->readPagesOfUserWithContent($id);
+                $user = new User($id, $firstname, $lastname, $email, $passwd, $validation, $displayname, $status, $pageList);
+                $error++;
+            }
+            
+            $preStmt->free_result();
+            $preStmt->close();
+            
+            if($error != 1){
+                return false;
+            }
+            return $user;
+        }
+
+
+        public function readGuestListOfUser($userId){
+            $userList = array();
+            $ownerId = "".$userId;
+            $sql = 'SELECT user.*
+                    FROM user, permission, page
+                    WHERE user.status = "guest"
+                    AND user.id = permission.user_id
+                    AND permission.page = page.id
+                    AND page.owner = ?';
+            $preStmt = $this->dbConnect->prepare($sql);
+            $preStmt->bind_param("s", $ownerId);
+            $preStmt->execute();
+            $preStmt->store_result();
+            $preStmt->bind_result($id, $firstname, $lastname, $email, $passwd, $validation, $displayname, $status);
+            
+            while($preStmt->fetch()){
+                $thisGuest = new User($id, $firstname, $lastname, $email, $passwd, $validation, $displayname, $status, null);
+    
+                $userList[] = $thisGuest;
+            }
+            
+            $preStmt->free_result();
+            $preStmt->close();
+            
+            return $userList;
+        }
+
+        public function readGuestListOfUserWithTheirPages($userId){ // does not load the actual content of the pages into the pages-array
+            $userList = array();
+            $ownerId = "".$userId;
+            $sql = 'SELECT user.*
+                    FROM user, permission, page
+                    WHERE user.status = "guest"
+                    AND user.id = permission.user_id
+                    AND permission.page = page.id
+                    AND page.owner = ?';
+            $preStmt = $this->dbConnect->prepare($sql);
+            $preStmt->bind_param("s", $ownerId);
+            $preStmt->execute();
+            $preStmt->store_result();
+            $preStmt->bind_result($id, $firstname, $lastname, $email, $passwd, $validation, $displayname, $status);
+            
+            $pageDAO = new PageDAO;
+            while($preStmt->fetch()){
+                $pageList = array();
+                $pageList = $pageDAO->readPagesOfUser($id);
+                $thisGuest = new User($id, $firstname, $lastname, $email, $passwd, $validation, $displayname, $status, $pageList);
+    
+                $userList[] = $thisGuest;
+            }
+            
+            $preStmt->free_result();
+            $preStmt->close();
+            
+            return $userList;
+        }
+
     }
 ?>
