1fed00fa6b232c551ebb7f7d9a9928a18ab36c62
Acidphreack <64482975+Acidphreack@users.noreply.github.com>
2020-05-05 00:37:01 +0200

    Add files via upload

--- views/AdminHome.php
+++ views/AdminHome.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <link href="./css/<?= $this->style ?>.css" rel="stylesheet" type="text/css"> <!-- $style in $this->style ändern, wenn es mit dem Command ausgeführt wird-->
+        <link rel="shortcut icon" type="image/x-icon" href="./favicon_96x96.png" size="48x48">
     </head>
     <body>
         <div id="header">
@@ -17,11 +18,12 @@
             <li><a href="index.php?cmd=RemoveUser">User löschen</a></li>
             <li><a href="index.php?cmd=RemoveGuest">Gäste löschen</a></li>
             <li><a href="index.php?cmd=AddAdmin">Admin anlegen</a></li>
+            <li><a href="index.php?cmd=AdminVerwaltung">Admins Verwalten</a></li>
             <li><a href="index.php?cmd=Logout">Logout</a></li>
         </ul>
         <div id="main">
             <div id="description">
-                Adminbereich.
+                Willkommen in der Admin-Verwaltung <?= $this->currentAdmin->getEmail() ?>.
             </div>
 
             <div id="content">
