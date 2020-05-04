b0fd7ed5125e3dde6a0640a252394309ea506245
Acidphreack <64482975+Acidphreack@users.noreply.github.com>
2020-05-05 00:44:24 +0200

    Add files via upload

--- views/AddAdmin.php
+++ views/AddAdmin.php
@@ -17,6 +17,7 @@
             <li><a href="index.php?cmd=AddUser">User anlegen</a></li>
             <li><a href="index.php?cmd=RemoveUser">User löschen</a></li>
             <li><a href="index.php?cmd=RemoveGuest">Gäste löschen</a></li>
+            <li><a href="index.php?cmd=AddAdmin">Admin anlegen</a></li>
             <li><a href="index.php?cmd=AdminVerwaltung">Admins Verwalten</a></li>
             <li><a href="index.php?cmd=Logout">Logout</a></li>
         </ul>
