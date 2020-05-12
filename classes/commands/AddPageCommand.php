2d76c5d1263a54bd127b22d4c0cb1e433e2fd741
Acidphreack <64482975+Acidphreack@users.noreply.github.com>
2020-05-12 02:44:54 +0200

    Add files via upload

--- classes/commands/AddPageCommand.php
+++ classes/commands/AddPageCommand.php
@@ -1,6 +1,7 @@
 <?php
     namespace classes\commands;
     use classes\model\User; // added um instanceof benutzen zu können.
+    use classes\model\Page;
     use classes\request\Request;
     use classes\response\Response;
     use classes\template\HtmlTemplateView;
@@ -31,6 +32,37 @@
 
             $pageList = $currentUser->getPages();
 
+            $allertText = "";
+            if(count($pageList) < 10){ // Seiten Anzahl auf 10 ? begrenzt
+                if($request->issetParameter('createPage')){
+                    $requestedTitle = $request->getParameter('pageInput');
+                    if($requestedTitle != ""){
+                        //create Page
+                        // db-stuff
+                        $pageDAO = new PageDAO;
+                        $newPage_id = $pageDAO->createPage($currentUser->getId(), $requestedTitle);
+                        $pageDAO->setPermissionForPage($currentUser->getId(), $newPage_id);
+                        // object-stuff
+                        $newPage = new Page($newPage_id, $currentUser->getId(), $requestedTitle,array());
+                        $pageList[] = $newPage;
+                        $currentUser->setPages($pageList);
+                        unset($_SESSION['user']);
+                        $_SESSION['user'] = serialize($currentUser);
+                        // send off to edit the new page
+                        header('location: ./?cmd=EditPage&page='.(count($pageList) -1));
+                        exit;
+                    }
+                    else{
+                        $allertText .= '<div style="color:red; text-align: center;">Sie müssen Ihrer Seite einen Namen geben!</div>';
+                    }
+
+                }
+            }
+            else{
+                $allertText .= '<div style="color:red; text-align: center;">Portfolios sind derzeit auf maximal 10 Seiten begrenzt!</div>';
+            }
+  
+
             // Hier muss noch einiges rein damit der User seinen Account und seine Pages verwalten kann. Seiten bearbeiten macht er an anderer Stelle
 
 
@@ -45,6 +77,7 @@
             $template->assign('style', $style);
             $template->assign('pageList', $pageList);
             $template->assign('displayname', $displayname);
+            $template->assign('allertText', $allertText);
             $template->render( $request, $response);
         }
     }
