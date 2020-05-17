0710a9201ec0a8e3a5906049193e950c25001952
Acidphreack <64482975+Acidphreack@users.noreply.github.com>
2020-05-17 17:39:03 +0200

    Add files via upload

--- classes/commands/AddUserCommand.php
+++ classes/commands/AddUserCommand.php
@@ -72,15 +72,26 @@
             $title = "Home";
 
             $content = "defaultContent";
-            $contentDescription = '<h1 id="defaultContent">Willkommen '.$firstname." ".$lastname. '</h1>';
-            $contentDescription .= '<h3>Diese Seite wird automatisch all Ihren Gästen zur Begrüßung angezeigt.<br>';
-            $contentDescription .= 'Sie sollten also zuallererst diese Seite bearbeiten und nach Ihren Wünschen gestalten.<br>';
-            $contentDescription .= 'Wenn Sie danach weitere Seiten eingerichtet haben, so können Sie diese für ausgewählte ';
+            $contentDescription = '<div>';
+            $contentDescription .= '<h1 id="defaultContent" style="text-align: center;">Herzlich willkommen '.$firstname." ".$lastname. '</h1>';
+            $contentDescription .= '<div style="font-size: 1.2rem;"><strong>Diese Seite wird automatisch all Ihren Gästen zur Begrüßung angezeigt ❗ </strong></div>';
+            $contentDescription .= '<div style="padding-left: 2rem;">Sie sollten also zuallererst diese Seite bearbeiten und nach Ihren Wünschen gestalten.</div>';
+            $contentDescription .= '<div style="padding-left: 2rem;">Wenn Sie weitere Seiten eingerichtet haben, können Sie diese für ausgewählte ';
             $contentDescription .= 'Gäste freigeben. Dieser Gast hat dann über die Navigationsleiste die möglichkeit ';
-            $contentDescription .= 'diese Seite(n) zu öffnen.<br>';
-            $contentDescription .= 'Sie haben neben der Möglichkeit Texte zu schreiben auch die Option ';
-            $contentDescription .= 'Bilder zur Dekoration einzufügen und Pdf-Dateien, sowie links auf all Ihren Seiten zu hinterlegen.';
-            $contentDescription .= 'Wir wünschen Ihnen viel Spaß beim erstellen Ihres Portfolios</h3>';
+            $contentDescription .= 'diese Seite(n) zu öffnen.</div>';
+            $contentDescription .= '<div style="padding-left: 2rem;">Sie haben neben der Möglichkeit Texte zu schreiben auch die Option ';
+            $contentDescription .= 'Bilder zur Dekoration einzufügen und Pdf-Dateien, sowie links auf all Ihren Seiten zu hinterlegen.</div>';
+            $contentDescription .= '<br>';
+            $contentDescription .= '<div style="font-size: 1.2rem;"><strong>Wenn Sie Ihrer Seite Inhalt hinzufügen, können Sie in den Textfeldern auch html verwenden.</strong></div>';
+            $contentDescription .= '<div style="padding-left: 2rem;">Wir empfehlen zur bestmöglichen Gestaltungsfreiheit das Nutzen von internen Style-sheets (&lt;style&gt;&lt;/style&gt;) oder Inline Styles.</div>';
+            $contentDescription .= '<div style="padding-left: 2rem;">Hier eine gute Quelle zu html/css: <a href="https://wiki.selfhtml.org/wiki/Startseite" target="_blank">selfhtml</a></div>';
+            $contentDescription .= '<div style="padding-left: 2rem;">Sie haben 65,535 Zeichen pro Inhalt zur verfügung.</div>';
+            $contentDescription .= '<br>';
+            $contentDescription .= '<div style="font-size: 1.2rem;"><strong>Bei jedem neuen Konto empfehlen wir dringend Ihr Kennwort zu ändern!</strong></div>';
+            $contentDescription .= '<div style="padding-left: 2rem;">Der Administrator der Ihr Konto angelegt hat, könnte Ihr Passwort nicht sicher gewählt, bzw es sich gemerkt haben.</div>';
+            $contentDescription .= '<div style="padding-left: 2rem;">Sie finden die Option zum Passwort-ändern in den Einstellungen unter Profil bearbeiten</div>';
+            $contentDescription .= '<h3>Wir wünschen Ihnen viel Spaß beim erstellen Ihres Portfolios</h3>';
+            $contentDescription .= '</div>';
             $html_id = "defaultContent";
 
             $belongsToPageNr = $pageDAO->createPage($ownerId, $title);
